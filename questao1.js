91c939dfc6fd931cfa45f30e3658c348c38f2646
carisbe1777 <carisbe066@gmail.com>
2025-04-09 17:02:28 +0000

    finalizado

---
+++ questao1.js
@@ -0,0 +1,12 @@
+const endereco = {
+    logradouro: "rua a",
+    cep: "46500000",
+    estado: "Bahia",
+    municipio: "Macaubas",
+    contato: {
+        nome: "isabelly",
+        telefone: "77999580599",
+        whatsapp:"77999580599",
+    }
+}
+console.log(endereco.contato.whatsapp)
